7a4d21104483cc7d48ac74f3415d314bc1cde2b1
孙伟红 <sunweihong@zichan360.com>
2020-10-14 17:26:19 +0800

    增加列表转树的结构公共代码

---
+++ src/main/java/com/zichan360/bigdata/dataportalcommons/common/utils/common/CommonUtils.java
@@ -0,0 +1,75 @@
+package com.zichan360.bigdata.dataportalcommons.common.utils.common;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
+/**
+ * CommonUtils
+ *
+ * @author sunweihong
+ * @desc 公共工具类
+ * @date 2020/10/14 17:13
+ **/
+public class CommonUtils {
+
+    /**
+     * 从列表数据生成树状json
+     *
+     * @param datas 列表数据
+     * @param rootNodeValue 根节点唯一标识的值
+     * @param propertyNameOfRelationNode 两级相关联的属性名称
+     * @param idName tree node的唯一标识
+     * @return 树状json
+     */
+    public static JSONArray generateTreeFromList(List<?> datas, String rootNodeValue, String propertyNameOfRelationNode, String idName) {
+        JSONObject root = new JSONObject();
+        root.put(idName, rootNodeValue);
+        root.put("child", new JSONArray());
+        for (Object data : datas) {
+            JSONObject jsonNode = JSON.parseObject(JSON.toJSONString(data));
+            String relationPropertyVaule = jsonNode.getString(propertyNameOfRelationNode);
+            JSONObject treeNode = findTreeNode(root, idName, relationPropertyVaule);
+            JSONArray child = new JSONArray();
+            if(treeNode.containsKey("child")) {
+                child = treeNode.getJSONArray("child");
+            }
+            child.add(jsonNode);
+            treeNode.put("child",child);
+        }
+        return root.getJSONArray("child");
+    }
+
+
+    /**
+     * 从树状json中遍历寻找node节点
+     * @param originObject 树状json对象
+     * @param idName 唯一标识的属性
+     * @param idValue 唯一标识的值
+     * @return 查找的节点
+     */
+    public static JSONObject findTreeNode(JSONObject originObject, String idName, String idValue) {
+        JSONObject result = null;
+        String nodeIdVaule = originObject.getString(idName);
+        if (idValue.equals(nodeIdVaule)) {
+            //如果要查找的是当前对象
+            result = originObject;
+        } else {
+            //去子节点中查询
+            if(originObject.containsKey("child")){
+                JSONArray nodeChilds = originObject.getJSONArray("child");
+                for (int i = 0; i < nodeChilds.size(); i++) {
+                    JSONObject childNode = nodeChilds.getJSONObject(i);
+                    JSONObject nextLevelChildNode = findTreeNode(childNode, idName, idValue);
+                    if (nextLevelChildNode != null) {
+                        result = nextLevelChildNode;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
